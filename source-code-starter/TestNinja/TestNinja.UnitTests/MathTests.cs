76cdffff58151a7c50a8cb59819fc15f82278e6c
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-09-16 17:21:39 +0300

    2.23 Parameterized Tests

--- source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
+++ source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -19,41 +19,21 @@ namespace TestNinja.UnitTests
         [Test]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
-            // Arrange
-
-            // Act
             var result = _math.Add(1, 2);
 
-            // Assert
             Assert.That(result, Is.EqualTo(3));
         }
 
         [Test]
-        public void Max_FirstArgumentIsGreater_ReturnsTheFirstArgument()
+        [TestCase(2,1,2)]
+        [TestCase(1,2,2)]
+        [TestCase(1,1,1)]
+        public void Max_WhenCalled_ReturnsTheGreaterArgument(int a, int b, int expectedResult)
         {
-
-            var result = _math.Max(2, 1);
-
-            Assert.That(result, Is.EqualTo(2));
+            var result = _math.Max(a, b);
+                
+            Assert.That(result, Is.EqualTo(expectedResult)); 
         }   
-
-        [Test]
-        public void Max_SecondArgumentIsGreater_ReturnsTheSecondArgument()
-        {
-
-            var result = _math.Max(1, 2);
-
-            Assert.That(result, Is.EqualTo(2));
-        }
-
-        [Test]
-        public void Max_ArgumentsAreEqual_ReturnsTheSameArgument()
-        {
-
-            var result = _math.Max(1, 1);
-
-            Assert.That(result, Is.EqualTo(1));
-        }
     }   
 }
     
