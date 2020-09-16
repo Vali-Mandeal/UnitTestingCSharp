07c9f77ef3409727866fa57f5312f1b498843a50
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-09-16 17:00:44 +0300

    2.20 Writing a Simple Unit Test

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            // Arrange
+            var math = new Math();
+
+            // Act
+            var result = math.Add(1, 2);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
