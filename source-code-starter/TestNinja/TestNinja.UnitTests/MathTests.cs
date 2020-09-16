af94d07c22f0b89dc834a96878d44a6a9f43d548
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-09-16 17:10:53 +0300

    2.21 Black Box Testing

--- source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
+++ source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -23,5 +23,36 @@ namespace TestNinja.UnitTests
             // Assert
             Assert.That(result, Is.EqualTo(3));
         }
-    }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnsTheFirstArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(2, 1);
+
+            Assert.That(result, Is.EqualTo(2));
+        }   
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnsTheSecondArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnsTheSameArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 1);
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }   
 }
+    
