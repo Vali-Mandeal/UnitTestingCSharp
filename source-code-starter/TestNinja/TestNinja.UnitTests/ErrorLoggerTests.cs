ffd4b5543268e75c64f3fade46c78455df8dc73d
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-09-17 12:19:19 +0300

    3.34 Testing Methods that Throw Exceptions

--- source-code-starter/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ source-code-starter/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -11,14 +11,29 @@ namespace TestNinja.UnitTests
     [TestFixture]
     class ErrorLoggerTests
     {
+        private ErrorLogger _errorLogger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _errorLogger = new ErrorLogger();
+        }
+
         [Test]
         public void Log_WhenCalled_SetTheLastErrorProperty()
         {
-            var logger = new ErrorLogger();
+            _errorLogger.Log("a");
 
-            logger.Log("a");
+            Assert.That(_errorLogger.LastError, Is.EqualTo("a"));
+        }
 
-            Assert.That(logger.LastError, Is.EqualTo("a"));
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            Assert.That(() => _errorLogger.Log(error), Throws.ArgumentNullException);
         }
     }
 }
