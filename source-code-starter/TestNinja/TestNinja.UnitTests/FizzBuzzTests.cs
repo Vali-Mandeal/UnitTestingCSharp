f26849045f32246004759234611aa6fe598644c3
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-10-01 12:31:48 +0300

    4.39 Exercise - FizzBuzz

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]  
+        public void GetOutput_InputIsDivisibileBy3And5_ReturnsFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3Only_ReturnsFizz()
+        {   
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy5Only_ReturnsBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsNotDivisibileBy3And5_ReturnsFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(1);
+
+
+            Assert.That(result, Is.EqualTo("1"));
+        }
+    }
+}
+    
