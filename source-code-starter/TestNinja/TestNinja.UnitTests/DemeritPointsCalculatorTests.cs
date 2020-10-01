2ed845c659a150c8173b855600bcc99707ab47e9
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-10-01 13:51:28 +0300

    4.41 & 4.43 Exercises - DemeritsPointsCalculator & Stack Tests

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _demeritPointsCalculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _demeritPointsCalculator = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void
+            CalculateDemeritsPoints_SpeedIsOutOfRange_ThrowsArgumentOutOfRangeException(int speed)
+        {   
+            Assert.That(() => 
+                _demeritPointsCalculator    
+                    .CalculateDemeritPoints(
+                        speed), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDemeritsPoints_WhenCalled_ReturnsDemeritPoints(int speed, int points)
+        {
+            var result = _demeritPointsCalculator.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(points));
+        }   
+    }       
+}
---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        //private Stack<string> _stack;
+        //[SetUp]
+        //public void SetUp()
+        //{
+        //    _stack = new Stack<string>();
+        //}
+
+        [Test]
+        public void Push_ArgIsNull_ThrowsArgNullException()
+        {
+            var stack = new Stack<string>();
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+            
+        [Test]
+        public void Push_ValidArg_AddsValue()
+        {
+            var stack = new Stack<string>();
+
+            stack.Push("a");
+
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Count_EmptyStack_ReturnsZero()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowsInvalidOperationException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObjects_ReturnsObjectOnTheTop()
+        {
+            // Arrange
+            var stack = new Stack<string>();
+
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            // Act
+            var result = stack.Pop();
+
+            // Assert
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObjects_RemovesObjectOnTheTop()
+        {
+            // Arrange
+            var stack = new Stack<string>();
+
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            // Act
+            stack.Pop();
+
+            Assert.That(stack.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Peek_EmptyStack_ThrowsInvalidOperationException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_StackWithAFewObjects_ReturnsObjectOnTheTop()
+        {
+            // Arrange
+            var stack = new Stack<string>();
+
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            // Act
+            var result = stack.Peek();
+
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Peek_StackWithAFewObjects_DoesNotRemoveObjectOnTheTop()
+        {
+            // Arrange
+            var stack = new Stack<string>();
+                
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            // Act
+            stack.Peek();
+
+            Assert.That(stack.Count, Is.EqualTo(3));
+        }
+    }
+}   
+    
