8cefb1fa2f733db70e5049dd2d354fdf1d30c7d2
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-10-19 12:56:42 +0300

    7.71 Extracting the IBooking Repository

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class BookingHelperTests_OverlappingBookingExists
+    {
+        [Test]
+        public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnsEmptyString()
+        {
+
+        }
+    }
+}
+    
